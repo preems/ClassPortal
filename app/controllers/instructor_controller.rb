132809d6819758e66e2258a061ed7a72af23e876
Krunal Gala <krunal3103@gmail.com>
2016-02-18 19:53:05 -0500

    instructor requests

--- app/controllers/instructor_controller.rb
+++ app/controllers/instructor_controller.rb
@@ -10,10 +10,7 @@ class InstructorController < ApplicationController
     @c = @cids.map {  |i| i.id }
     @pending = Request.all
     @pending = @pending.select{ |i| i.course_id.in?(@c) }
-
-    #@joined =Request.joins(:user).where
   end
-
 
   def enroll
     a=params[:user_id]
@@ -21,17 +18,13 @@ class InstructorController < ApplicationController
     one=User.find(a)
     two=Course.find(b)
     one.courses << two
-    #@byebye= Request.where("course_id = ? AND user_id = ?", b, a)
     Request.where(course_id:b,user_id:a).first.destroy
     redirect_to requests_path
   end
 
-
   def requestinactive
     @course= Course.find(params[:id])
-    #@course.inactive=1
     @course.update_attribute(:inactive,1)
-
     redirect_to mycourses_path
   end
 end
