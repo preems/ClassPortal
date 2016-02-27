271058bc66cc90c7c9ac3a7c69eb207cedeabfcc
Krunal Gala <krunal3103@gmail.com>
2016-02-26 20:32:04 -0500

    search fix

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -4,7 +4,7 @@ class CourseController < ApplicationController
     if params[:search]
       @course = Course.search(params[:search])
     else
-      @course = nil
+      @course = Course.search("")
     end
   end
 
